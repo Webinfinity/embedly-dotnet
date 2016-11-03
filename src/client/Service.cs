7fa2b559255a453776b275e5e74b783ded30b015
Sergey Svirsky <sergey.svirsky@itechart-group.com>
2016-11-03 15:01:07 +0000

    EnhancedWebClient that can automatically decompress result (server returns gzip from time to time)

--- src/client/Service.cs
+++ src/client/Service.cs
@@ -1,71 +1,87 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Runtime.Serialization.Json;
+using System.Text;
 using Common.Logging;
 
 namespace Embedly
 {
-    /// <summary>
-    /// Retrieves the list of supported services and provides a way to check if a URL is supported
-    /// </summary>
-    /// <remarks>
-    /// This uses a string splitting and matching algorithm to check for supported URLs which is
-    /// significantly faster than using regular expressions
-    /// </remarks>
-    internal sealed class Service
-    {
-        private const string ServiceApi = "http://api.embed.ly/1/services";
-        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+	/// <summary>
+	/// Retrieves the list of supported services and provides a way to check if a URL is supported
+	/// </summary>
+	/// <remarks>
+	/// This uses a string splitting and matching algorithm to check for supported URLs which is
+	/// significantly faster than using regular expressions
+	/// </remarks>
+	internal sealed class Service
+	{
+		private const string ServiceApi = "http://api.embed.ly/1/services";
+		private static readonly ILog Log = LogManager.GetCurrentClassLogger();
 
-        // singleton pattern from: http://csharpindepth.com/Articles/General/Singleton.aspx
-        private static readonly Lazy<Service> Lazy = new Lazy<Service>(() => new Service());
+		// singleton pattern from: http://csharpindepth.com/Articles/General/Singleton.aspx
+		private static readonly Lazy<Service> Lazy = new Lazy<Service>(() => new Service());
 
-        private Service()
-        {
-            DownloadServices();
-        }
+		private Service()
+		{
+			DownloadServices();
+		}
 
-        internal static Service Instance
-        {
-            get { return Lazy.Value; }
-        }
+		internal static Service Instance
+		{
+			get { return Lazy.Value; }
+		}
 
-        internal IEnumerable<Provider> Providers { get; private set; }
+		internal IEnumerable<Provider> Providers { get; private set; }
 
-        private void DownloadServices()
-        {
-            Log.InfoFormat("Loading embed.ly service list");
+		private void DownloadServices()
+		{
+			Log.InfoFormat("Loading embed.ly service list");
 
-            try
-            {
-                using (var client = new WebClient())
-                {
-                    byte[] data = client.DownloadData(ServiceApi);
-                    DeserializeServices(data);
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.ErrorFormat("Error downloading embed.ly service list", ex);
-            }
-        }
+			try
+			{
+				using (var client = new EnhancedWebClient())
+				{
+					client.Encoding = Encoding.UTF8;
+					byte[] data = client.DownloadData(ServiceApi);
 
-        private void DeserializeServices(byte[] data)
-        {
-            try
-            {
-                using (var stream = new MemoryStream(data))
-                {
-                    var serializer = new DataContractJsonSerializer(typeof (IEnumerable<Provider>));
-                    Providers = (IEnumerable<Provider>)serializer.ReadObject(stream);
-                }
-            }
-            catch (Exception ex)
-            {
-                Log.ErrorFormat("Error deserializing embed.ly service list", ex);
-            }
-        }
-    }
+					DeserializeServices(data);
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.ErrorFormat("Error downloading embed.ly service list", ex);
+			}
+		}
+
+		private void DeserializeServices(byte[] data)
+		{
+			try
+			{
+				using (var stream = new MemoryStream(data))
+				{
+					var serializer = new DataContractJsonSerializer(typeof(IEnumerable<Provider>));
+					Providers = (IEnumerable<Provider>)serializer.ReadObject(stream);
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.ErrorFormat("Error deserializing embed.ly service list", ex);
+			}
+		}
+
+		private class EnhancedWebClient : WebClient
+		{
+			protected override WebRequest GetWebRequest(Uri address)
+			{
+				var request = base.GetWebRequest(address) as HttpWebRequest;
+				if (request == null) return null;
+
+				request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+				return request;
+			}
+		}
+	}
 }
